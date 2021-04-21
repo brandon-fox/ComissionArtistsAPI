431619564d989aa4570ebe4175a46ee9786d1c67
Brandon Fox <bfox@foxtrot.email>
2021-04-21 10:43:05 -0400

    Add MVC Test for mocked request to Artists

--- src/test/java/org/vindicta/artists/HttpRequestTest.java
+++ src/test/java/org/vindicta/artists/HttpRequestTest.java
@@ -2,27 +2,44 @@ package org.vindicta.artists;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class HttpRequestTest {
+    public static final String ARTISTS = "/artists";
+    public static final String LOCALHOST = "http://localhost:";
     @LocalServerPort
     private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     public void artistsShouldReturn200Response() {
-        ResponseEntity<String> response = this.restTemplate.getForEntity("http://localhost:" + port + "/artists", String.class);
+        ResponseEntity<String> response = this.restTemplate.getForEntity(LOCALHOST + port + ARTISTS, String.class);
         assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+
+    @Test
+    public void shouldReturnOKResponse() throws Exception {
+        this.mockMvc.perform(get(ARTISTS)).andDo(print()).andExpect(status().isOk());
     }
 }
